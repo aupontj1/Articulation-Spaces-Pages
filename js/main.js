456f6629183cde986981ab6f0a22ef14773ed7ba
aupontj1 <aupontj01@gmail.com>
2024-07-20 16:40:46 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const menuToggle = document.querySelector('.menuToggle');
+const navigation = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', function() {
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+
+    if(menuToggle.classList.contains('active')){
+        menuToggle.innerHTML = '<i class="fas fa-times"></i>'
+    } else {
+        menuToggle.innerHTML = '<i class="fas fa-bars"></i>';
+    }
+
+});
+
+
+
+
+
